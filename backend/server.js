776a9fa59800cd423a851360c5b8b67859c2f457
Dayana Torres <luzdayanatorres@gmail.com>
2025-04-04 00:45:52 -0500

    Update

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const storesRouter = require('./routes/stores');
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5175",
+    origin: "http://localhost:5178",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
