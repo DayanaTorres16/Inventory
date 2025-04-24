bd1dfe90de611a3fb3fb09ca49fc6eba901255be
Dayana Torres <luzdayanatorres@gmail.com>
2025-04-24 13:18:43 -0500

    CSS improvements

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,15 +3,13 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-        <footer className='footer'>
-            <p>Creadoras del sistema de gestión Inventarios de Alfa y Omega:</p>
-            <div className='creators'>
-                <p>Dayana Torres</p>
-                <p>Valentina Rojas</p>
-            </div>
-        </footer>
-    </div>
+    <footer className='footer'>
+        <p>Creadoras del sistema de gestión Inventarios de Alfa y Omega:</p>
+        <div className='creators'>
+            <p>Dayana Torres</p>
+            <p>Valentina Rojas</p>
+        </div>
+    </footer>
   )
 }
 
